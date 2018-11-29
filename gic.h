843e135352b3f27aa214f82c578c6f89cc1fc5b5
Max Base <MaxBaseCode@Gmail.Com>
2018-11-29 16:28:38 +0330

    Update gic.h

--- gic.h
+++ gic.h
@@ -1,1 +1,20 @@
-
+/**
+*
+* @Name : Gic
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2018-11-29
+* @Released under : https://github.com/BaseMax/Gic/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/Gic
+*
+**/
+#include <stdio.h>
+//#include <unistd.h>
+#define GIC_VERSION 	"0.1 beta"
+#define CONSOLE_RESET	"\x1b[0m"
+#define CONSOLE_GREEN	"\x1b[32m"
+#define CONSOLE_YELLOW	"\x1b[33m"
+#define CONSOLE_RED		"\x1b[31m"
+#define CONSOLE_BLUE	"\x1b[34m"
+#define CONSOLE_CYAN	"\x1b[36m"
+#define CONSOLE_MAGENTA	"\x1b[35m"
