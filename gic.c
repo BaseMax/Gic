28764814a9fbf387a27b65eccf4d59f2986f9906
Max Base <MaxBaseCode@Gmail.Com>
2018-11-29 17:04:38 +0330

    Update gic.c

--- gic.c
+++ gic.c
@@ -12,18 +12,19 @@
 
 int main(int argc,char** argv)
 {
-	puts(CONSOLE_GREEN "Git Control (" GIC_VERSION ")");
-	puts("");
-	puts("    Management the repositories easily in our directory.");
-	puts("    Open-source <https://github.com/BaseMax/Gic>");
-	puts("    Usage: gic <command> [arguments]");
-	puts("");
-	puts(" These are commands :");
-	puts("");
-	puts(CONSOLE_YELLOW "  clone  " CONSOLE_MAGENTA "    Clone a repository into disk");
-	puts(CONSOLE_YELLOW "  help   " CONSOLE_MAGENTA "    View the help text");
-	puts(CONSOLE_YELLOW "  version" CONSOLE_MAGENTA "    Print the version of GIC");
-	puts(CONSOLE_RESET);
 	Argument argument = arguments_parse(argc,argv);
+	if(argument.type == CommandHelp)
+	{
+		help_init();
+		help_commands();
+	}
+	else if(argument.type == CommandVersion)
+	{
+		help_init();
+	}
+	else if(argument.type == CommandClone)
+	{
+		puts("...");
+	}
 	return 0;
 }
