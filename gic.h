dd2605cba951b049c706522a9e1bc8532156457e
Max Base <MaxBaseCode@Gmail.Com>
2018-11-29 16:43:24 +0330

    Update gic.h

--- gic.h
+++ gic.h
@@ -8,13 +8,50 @@
 * @Repository : https://github.com/BaseMax/Gic
 *
 **/
+#ifndef GIC_LIB
+#define GIC_LIB
 #include <stdio.h>
 //#include <unistd.h>
-#define GIC_VERSION 	"0.1 beta"
-#define CONSOLE_RESET	"\x1b[0m"
-#define CONSOLE_GREEN	"\x1b[32m"
-#define CONSOLE_YELLOW	"\x1b[33m"
-#define CONSOLE_RED		"\x1b[31m"
-#define CONSOLE_BLUE	"\x1b[34m"
-#define CONSOLE_CYAN	"\x1b[36m"
-#define CONSOLE_MAGENTA	"\x1b[35m"
+/*
+ * General Value for GIC
+ */
+#define GIC_VERSION 		"0.1 beta"
+#define GIC_VERSION_CODE	1
+/*
+ * Colorize the console text
+ */
+#define CONSOLE_RESET		"\x1b[0m"
+#define CONSOLE_GREEN		"\x1b[32m"
+#define CONSOLE_YELLOW		"\x1b[33m"
+#define CONSOLE_RED			"\x1b[31m"
+#define CONSOLE_BLUE		"\x1b[34m"
+#define CONSOLE_CYAN		"\x1b[36m"
+#define CONSOLE_MAGENTA		"\x1b[35m"
+
+/*
+ * Enum CommandType
+ * Used in Parse the agument(s)
+ */
+typedef enum
+{
+	CommandHelp				= 0,
+	CommandVersion			= 1,
+	CommandClone			= 2,
+} CommandType;
+typedef struct
+{
+	//...
+} CommandFlags;
+typedef struct
+{
+	CommandType 	type;
+	char* 			value;
+	CommandFlags 	flags;
+} Argument;
+
+/*
+ * Requirement , functions
+ */
+Argument arguments_parse(int argc,char** argv);
+
+#endif
